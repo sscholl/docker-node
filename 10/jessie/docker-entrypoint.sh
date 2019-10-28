d15e00f03739481334651490bb2b3cbfc9b1bd0b
Simon Eric Scholl <sscholl@users.noreply.github.com>
2019-10-28 08:42:07 +0100

    modify passwd if on openshift

--- 10/jessie/docker-entrypoint.sh
+++ 10/jessie/docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+# Openshift: Ensure that assigned uid has entry in /etc/passwd.
+if [ `id -u` -ge 10000 ]; then
+  cat /etc/passwd | sed -e "s/^node:/builder:/" > /tmp/passwd
+  echo "node:x:`id -u`:`id -g`:,,,:/home/node:/bin/bash" >> /tmp/passwd
+  cat /tmp/passwd > /etc/passwd
+  rm /tmp/passwd
+fi
+
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   set -- node "$@"
 fi
